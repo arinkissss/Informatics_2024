866c789d56112cc77a4eb2194ded4ef83f0e3e56
Egor_scahov <log69555@gmail.com>
2024-10-30 13:40:17 +0300

    lab4

--- golang/lab4/calc.go
+++ golang/lab4/calc.go
@@ -1,26 +1,35 @@
-package lab4
+package Lab4
 
 import (
-	"math"
-	"fmt"
+  "fmt"
+  "math"
 )
 
-func Calculator(a, x float64) float64{
-	y := math.Tan(math.Pow(math.Log10(a+x),3)/math.Pow(a+x,2/7))
-	return y
+func Calculate(x, a float64) float64 {
+  y := math.Tan(math.Pow(math.Log10(a+x),3))/math.Pow((a+x)*(a+x),1.0/7.0)
+  return y
 }
 
-func TaskA(a, Xn, Xk, delX float64) {
-	for x := Xn; x <= Xk; x += delX {
-		fmt.Println(Calculator(a, x))
-	}
+func TaskA(b, Xn, Xk, delX float64) []float64 {
+  var y []float64
+  for x := Xn; x <= Xk; x += delX {
+    y = append(y, Calculate(b, x))
+  }
+  return y
 }
 
-func TaskB(a float64, x[5]float64) {
-	for _, value := range x {
-		fmt.Println(Calculator(a, value))
+func TaskB(b float64, x [5]float64) []float64 {
+  var y []float64
+  for _, value := range x {
+    y = append(y, Calculate(b,value))
+  }
+  return y
+}
 
-	
-	}
+func RunLab4() {
+  a := 2.0
+  fmt.Println(TaskA(a, 1.08, 1.88, 0.16))
+  var s = [5]float64{1.16, 1.35, 1.48,1.52, 1.96}
+  fmt.Println(TaskB(a, s))
 }
 
--- golang/main.go
+++ golang/main.go
@@ -1,10 +1,10 @@
 package main
 
 import "isuct.ru/informatics2022/lab4"
+import "fmt"
 
-const a = 2.0
 func main() {
-	lab4.TaskA(a, 1.08, 1.88, 0.16)
-	var s = [5]float64{1.16, 1.35, 1.48, 1.52, 1.96}
-	lab4.TaskB(a, s)
+	fmt.Println("Савельева Арина Денисовна")
+
+	Lab4.RunLab4()
 }

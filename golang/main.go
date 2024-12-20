ac668802325f93ced87a9922959c7ff30892d127
arinkissss <saveleva.77@icloud.com>
2024-12-21 00:12:30 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -4,7 +4,7 @@ import (
   "fmt"
   "isuct.ru/informatics2022/lab4"
   "isuct.ru/informatics2022/lab6"
-  "isuct.ru/informatics2022/lab7"
+  lab7 "isuct.ru/informatics2022/labs/lab7"
 )
 
 func main() {

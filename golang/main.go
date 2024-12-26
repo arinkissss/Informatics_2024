ab6e512a21c47e9a73f5fd0de3932d42ccc008c6
arinkissss <saveleva.77@icloud.com>
2024-12-26 19:10:20 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -3,11 +3,9 @@ package main
 import (
   "fmt"
   "isuct.ru/informatics2022/lab6"
-  "isuct.ru/informatics2022/labs/lab4"
 )
 
 func main() {
   fmt.Println("Савельева Арина Денисовна")
-  lab4.RunLab4()
   lab6.RunLab6()
 }

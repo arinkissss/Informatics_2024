bca0dba8aea98c7cea9c9a6fd221f450642017ba
A <a@mail.ru>
2024-12-27 23:24:28 +0300

    lab 6

--- golang/lab6/lab.go
+++ golang/lab6/lab.go
@@ -1,13 +1,6 @@
 package lab6
 
-import (
-	"fmt"
-)
-
-type TV interface {
-	GetChannel() int
-	SetChannel(channel int)
-}
+import "fmt"
 
 type Television struct {
 	channel int
@@ -18,27 +11,24 @@ func (t *Television) GetChannel() int {
 }
 
 func (t *Television) SetChannel(channel int) {
-	if channel > 0 {
-		t.channel = channel
-		fmt.Printf("Channel set to %d\n", channel)
-	} else {
-		fmt.Println("Invalid channel number")
-	}
+	(*t).channel = channel
 }
 
 func (t *Television) Display() {
+	channel := t.GetChannel()
 	fmt.Println("╔════════════════╗")
 	fmt.Println("║                ║")
-	fmt.Printf("║   Channel %2d   ║\n", t.GetChannel())
+	fmt.Println("║   Канал", channel, "    ║\n")
 	fmt.Println("║                ║")
 	fmt.Println("╚════════════════╝")
 }
 
 func RunLab6() {
-	tv := &Television{}
-	tv.SetChannel(5)
-	tv.Display()
+	var tv Television = Television{4}
+	var tvPointer *Television = &tv
+	tvPointer.SetChannel(5)
+	tvPointer.Display()
 
-	tv.SetChannel(12)
-	tv.Display()
+	tvPointer.SetChannel(12)
+	tvPointer.Display()
 }
--- golang/main.go
+++ golang/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
   "fmt"
+  "isuct.ru/informatics2022/lab4"
   "isuct.ru/informatics2022/lab6"
 )
 
 func main() {
   fmt.Println("Савельева Арина Денисовна")
+  lab4.RunLab4()
   lab6.RunLab6()
 }
